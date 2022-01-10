ef8964178332e0b00927d61b0120918e02718ee6
Sander Smedbol <sander.smedbol@stud.noroff.no>
2022-01-10 13:38:38 +0100

    lesson 1 and 2 + lesson tasks

---
+++ script.js
@@ -0,0 +1,72 @@
+// var myVar;
+// console.log(myVar);
+
+// var colors = ["red", "blue", "green"];
+// colors.push("purple"); //adding purple to "colors"
+// console.log(colors);
+// console.log(colors.length);
+
+// var cars = ["toyota", "volkswagen", "mercedes", "nissan", "bmw"];
+// console.log(cars[0]);
+// console.log(cars.length);
+
+// for (var i = 0; i < cars.length; i++) {
+//   console.log(i);
+//   console.log(cars[i]);
+// }
+
+// var selectedNumber = 60;
+// // 3+ => 10 // true
+// // 30 <= 50 // false
+// // ok the messege will show
+// if (selectedNumber >= 10 && selectedNumber <= 50) {
+//   console.log("The selected number is within range.");
+// }
+
+// var dayOfTheWeek = "Tuesday";
+// // "Monday" === "Monday" // true
+// // "Monday" === "Tuesday" // false
+
+// //true                        OR false
+// if (dayOfTheWeek === "Monday" || dayOfTheWeek === "Tuesday") {
+//   console.log("It's not the weekend");
+// }
+
+// var stringFour = "4";
+// var numberFour = 4;
+// if (stringFour === numberFour) {
+//   console.log("equal");
+// } else {
+//   console.log("not equal");
+// }
+// if (stringFour == numberFour) {
+//   console.log("equal");
+// } else {
+//   console.log("not equal");
+// }
+
+//lesson task 1
+// var animals = ["cow", "bird", "mouse"];
+// console.log(animals.length);
+// for (var i = 0; i < animals.length; i++) {
+//   console.log(i);
+// }
+
+//lesson task 2
+//== is almost alike, === is exact
+//"6" === 6 will be false/ not equal
+//"6" == 6 will be true/equal
+
+// var stringFour = "4";
+// var numberFour = 4;
+// if (stringFour !== numberFour) {
+//   console.log("not equal");
+// } else {
+//   console.log("equal");
+// }
+// if (stringFour != numberFour) {
+//   console.log("not equal");
+// } else {
+//   console.log("equal");
+// }
+// var someVar;
